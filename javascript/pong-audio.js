6a463a511e41f831e9f80bde631dcccc6db3c5d7
lightyearlaunchinglemur <144568447+FernandoRodriguez7@users.noreply.github.com>
2025-12-23 03:30:25 -0600

    Adjust adventure music volume to 1

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -60,7 +60,7 @@ ambientSound.player.volume.value = 0; //turn down volume
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = 1;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
